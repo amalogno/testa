470453c25720c54489605775c93ac988c967d22b
amalogno <amalogno@gmail.com>
2026-04-30 09:46:21 +1000

    Create models/Listing.js file

---
+++ models/Listing.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  seller: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true, 
+    ref: 'User' 
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Listing', listingSchema);
